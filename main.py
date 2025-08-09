23e8f0e2d95fe5c99db85bde1c8b596643b03d79
kunalyadav19 <kunalyadav2234@gmail.com>
2025-08-10 02:37:49 +0530

    f1

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
 #!/usr/bin/env python3
-import os, time
+import os
+import time
+import shutil
+import random
 
-# "KUNAL" pixel map (█ = filled, space = empty)
-KUNAL = [
-    "█   █ ████  █   █ █   █ ████ ",
-    "█   █ █   █ ██  █ ██  █ █    ",
-    "█   █ ████  █ █ █ █ █ █ █ ██ ",
-    "█   █ █     █  ██ █  ██ █   █",
-    " ███  █     █   █ █   █  ███ ",
-]
+# Snake display settings
+SNAKE_CHAR = "█"
+SNAKE_LENGTH = 10
+SPEED = 0.05  # seconds per frame
 
-def render(display):
-    os.system("clear")
-    for row in display:
-        print("".join(row))
+# Get terminal size
+def get_terminal_size():
+    size = shutil.get_terminal_size(fallback=(80, 24))
+    return size.columns, size.lines
 
-def zigzag_draw(name_pixels):
-    height = len(name_pixels)
-    width = len(name_pixels[0])
-    display = [[" " for _ in range(width)] for _ in range(height)]
-
-    # Zigzag traversal
-    for y in range(height):
-        if y % 2 == 0:  # left to right
-            x_range = range(width)
-        else:           # right to left
-            x_range = range(width - 1, -1, -1)
-
-        for x in x_range:
-            if name_pixels[y][x] == "█":
-                display[y][x] = "█"
-            render(display)
-            time.sleep(0.01)  # speed
+def clear():
+    os.system("cls" if os.name == "nt" else "clear")
 
 if __name__ == "__main__":
-    pixels = [list(row) for row in KUNAL]
+    width, height = get_terminal_size()
+    snake = [(x, 0) for x in range(SNAKE_LENGTH)]  # initial position
+    direction = (1, 0)  # moving right
+
     while True:
-        zigzag_draw(pixels)
-        time.sleep(0.5)  # pause before repeating
+        # Move snake
+        head_x, head_y = snake[-1]
+        new_x = (head_x + direction[0]) % width
+        new_y = (head_y + direction[1]) % height
+        snake.append((new_x, new_y))
+        if len(snake) > SNAKE_LENGTH:
+            snake.pop(0)
+
+        # Randomly change direction
+        if random.random() < 0.05:
+            direction = random.choice([(1, 0), (-1, 0), (0, 1), (0, -1)])
+
+        # Draw world
+        grid = [[" " for _ in range(width)] for _ in range(height)]
+        for x, y in snake:
+            grid[y][x] = SNAKE_CHAR
+
+        clear()
+        for row in grid:
+            print("".join(row))
+
+        time.sleep(SPEED)
+
